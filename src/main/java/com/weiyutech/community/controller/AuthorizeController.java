45b3bb79b7008d1800bf3abcc9d70b19437bedd6
kxl361 <kxl361@163.com>
2019-08-09 19:20:44 +0800

    hhh

---
+++ src/main/java/com/weiyutech/community/controller/AuthorizeController.java
@@ -0,0 +1,42 @@
+package com.weiyutech.community.controller;
+
+import com.weiyutech.community.dto.AccessTokenDto;
+import com.weiyutech.community.dto.GithubUser;
+import com.weiyutech.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String client_id;
+
+    @Value("${github.client.secret}")
+    private String client_secret;
+
+    @Value("${github.redirect.uri}")
+    private String redirect_uri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        AccessTokenDto accessTokenDto = new AccessTokenDto();
+        accessTokenDto.setClient_id(client_id);
+        accessTokenDto.setClient_secret(client_secret);
+        accessTokenDto.setCode(code);
+        accessTokenDto.setState(state);
+        accessTokenDto.setRedirect_uri(redirect_uri);
+        String accessToken = githubProvider.getAccessToken(accessTokenDto);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getId());
+        return "index";
+    }
+
+}
--- src/main/java/com/weiyutech/community/controller/HelloController.java
+++ src/main/java/com/weiyutech/community/controller/HelloController.java
@@ -3,10 +3,17 @@ package com.weiyutech.community.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
 
     @GetMapping("/hello")
     public String Hello(@RequestParam(name = "name", required = false, defaultValue = "world") String name, Model model) {
