fb6e6f459ceb1a639cf8226a23ca7e547b0bb51d
kxl361 <kxl361@163.com>
2019-08-12 19:20:06 +0800

    add db migration

--- src/main/java/com/weiyutech/community/controller/AuthorizeController.java
+++ src/main/java/com/weiyutech/community/controller/AuthorizeController.java
@@ -8,11 +8,11 @@ import com.weiyutech.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -36,7 +36,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request) {
+                           HttpServletResponse response) {
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(client_id);
         accessTokenDto.setClient_secret(client_secret);
@@ -48,14 +48,16 @@ public class AuthorizeController {
         if(null!=githubUser){
             System.out.println(githubUser.getId());
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUser.getName());
             user.setAccount_id(String.valueOf(githubUser.getId()));
             user.setGmt_create(System.currentTimeMillis());
             user.setGmt_modified(user.getGmt_create());
             userMapper.insert(user);
-            request.getSession().setAttribute("githubUser",githubUser);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
+            //request.getSession().setAttribute("githubUser",githubUser);
         }else{
             return "redirect:/";
         }
--- src/main/java/com/weiyutech/community/controller/HelloController.java
+++ src/main/java/com/weiyutech/community/controller/HelloController.java
@@ -1,23 +1,44 @@
 package com.weiyutech.community.controller;
 
+import com.weiyutech.community.mapper.UserMapper;
+import com.weiyutech.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HelloController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @RequestMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
         return "index";
     }
 
 
     @GetMapping("/hello")
     public String Hello(@RequestParam(name = "name", required = false, defaultValue = "world") String name, Model model) {
-        model.addAttribute("name",name);
+        model.addAttribute("name", name);
         return "hello";
     }
 }
